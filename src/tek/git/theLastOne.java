f31eb42e2b1179d1811e81c39adbae83c6b581a1
zibj <zibjamal@gmail.com>
2023-08-23 11:09:43 -0700

    change from new branch

---
+++ src/tek/git/theLastOne.java
@@ -0,0 +1,10 @@
+package tek.git;
+
+public class theLastOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/tek/git/thisOne.java
@@ -0,0 +1,11 @@
+package tek.git;
+
+public class thisOne {
+
+	public static void main(String[] args) {
+
+System.out.println("Look at the world");
+
+	}
+
+}
